66be0bffc169c45d2baf05530742659e9178b113
HaniehGRN <gorganihanieh@gmail.com>
2022-01-15 21:17:12 +0330

    add homepage and its styles

---
+++ src/homepage.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                
+            </div>
+        </div>
+    </div>
+);
