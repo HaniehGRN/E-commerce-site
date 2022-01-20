4962bfab90d2f5d9e66246b4fabff8e90f256f05
HaniehGRN <gorganihanieh@gmail.com>
2022-01-20 23:14:36 +0330

    creating Shop page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
@@ -7,7 +10,11 @@ function App() {
 
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
