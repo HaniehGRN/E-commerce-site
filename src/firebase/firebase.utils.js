0150ea77b3494b1a441d63ec55ef78631d974614
HaniehGorgani <gorganihanieh@gmail.com>
2022-03-09 16:30:27 +0330

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,13 +11,7 @@ import { initializeApp } from '@firebase/app';
 import { collection, getFirestore, doc, getDoc, setDoc, writeBatch } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCdVLy9jDsl2AOZH7oYqTM0o5xEervttb0",
-    authDomain: "crwn-clothing-ccf4a.firebaseapp.com",
-    projectId: "crwn-clothing-ccf4a",
-    storageBucket: "crwn-clothing-ccf4a.appspot.com",
-    messagingSenderId: "664201191093",
-    appId: "1:664201191093:web:8ab3a12c1e68ffcfd1e8fc",
-    measurementId: "G-Y0R4NKH6B9"
+   ///////////////////////////////////////
 };
 
 const app = initializeApp(firebaseConfig);
